be791da0bd8c776845ded18e1bd48f7a12f6788b
gaurav bisht <gauravengg.bisht@gmail.com>
2024-07-15 10:37:38 +0530

    emailjs service added

---
+++ main.js
@@ -0,0 +1,21 @@
+function sendEmail() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+
+    var templateParams = {
+      from_name: name,
+      from_email: email,
+      subject: subject,
+      message: message
+    };
+
+    emailjs.send('service_d8adm9h', 'template_wsplwvl', templateParams)
+      .then(function(response) {
+        alert('Email sent successfully!');
+        document.getElementById('contact-form').reset();
+      }, function(error) {
+        alert('Failed to send email. Please try again.');
+      });
+  }
